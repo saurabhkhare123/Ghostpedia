9161875c3feba4b8b5f755f8d1326bd3fd117e42
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2020-11-05 02:17:06 +0530

    first page fixed

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ window.addEventListener('load',()=>{
     const preload = document.querySelector ('.preload');
     preload.classList.add("preload-finish");
 })
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+    
+})
